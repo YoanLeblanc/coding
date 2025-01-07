1213d8a43989e840987158948cb5f24d81f05190
Yoan Leblanc <yoa.lebl@gmail.com>
2025-01-07 17:11:28 +0100

    solved leetcode problem 1408

---
+++ 1408-string-matching-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        n = len(words)
+        res = []
+        for i in range(n):
+            x = words[i]
+            for j in range(n):
+                if x in words[j] and i != j:
+                    res.append(x)
+                    break
+        return res
